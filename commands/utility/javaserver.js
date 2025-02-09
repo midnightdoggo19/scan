598a14fa0c3eb2bff7e23fc01a31ccdbb997def5
Midnight Doggo <git@midnightdoggo19.com>
2025-02-09 21:05:17 +0000

    starting to make it more embed-y

--- commands/utility/javaserver.js
+++ commands/utility/javaserver.js
@@ -16,10 +16,20 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.deferReply();
+
+        const response = await fetch(`https://api.mcstatus.io/v2/status/java/${interaction.options.getString('server')}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
         try {
             const widgetEmbed = new EmbedBuilder()
                 .setColor(0x0099FF)
                 .setTitle('Server Status')
+                .setAuthor({ name: 'Midnight Doggo', iconURL: 'https://avatars.githubusercontent.com/u/71900479?v=4' })
+                .setDescription(`Players online: ${String(data.players.max)}/${String(data.players.online)}`)
                 .setImage(`https://api.mcstatus.io/v2/widget/java/${interaction.options.getString('server')}:${interaction.options.getInteger('port') || 25565}`)
                 .setTimestamp()
 
