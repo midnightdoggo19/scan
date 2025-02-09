2826f88cad0f253524ff966a3c06ff9da7189978
Midnight Doggo <git@midnightdoggo19.com>
2025-02-09 21:35:00 +0000

    widget command

---
+++ commands/utility/widget.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('widget')
+        .setDescription('Get the widget of a Java server')
+        .addStringOption(option =>
+            option.setName('server')
+                .setDescription('The address of the server')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option.setName('port')
+                .setDescription('The port the server runs on (default 25565)')
+        ),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+        try {
+            const widgetEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle('Server Status')
+                .setImage(`https://api.mcstatus.io/v2/widget/java/${interaction.options.getString('server')}:${interaction.options.getInteger('port') || 25565}`)
+                .setTimestamp()
+
+            await interaction.editReply({ embeds: [widgetEmbed] });
+        } catch (error) {
+            console.error('Error:', error);
+            await interaction.editReply('Failed to fetch server widget.');
+        }
+    }
+};
