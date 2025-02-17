36efb9c633f29141b8bb2e69a180f2e5bf755c3f
Midnight Doggo <git@midnightdoggo19.com>
2025-02-17 01:02:30 +0000

    added command - check name uniqueness

---
+++ commands/utility/boring.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { logger } = require('../../functions.js');
+const levenshtein = require('js-levenshtein');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('boring')
+		.setDescription('See how unique a username is')
+        .addStringOption(option =>
+            option.setName('name')
+                .setDescription('The username')
+                .setRequired(true)
+        )
+        .setContexts(0, 1, 2)
+        .setIntegrationTypes(0, 1),
+
+    async execute(interaction) {
+        interaction.deferReply()
+
+        const name = interaction.options.getString('name');
+        const words = await fetch('https://matdoes.dev/minecraft-uuids/api/words.txt') + await fetch('https://matdoes.dev/minecraft-uuids/api/suffixes.txt');
+        const lev = await String(levenshtein(name, String(words)));
+
+        if (lev >= 100) {
+            await interaction.editReply('This is just some random keysmashing!');
+        }
+        else {
+            const score = 100 - lev;
+            await interaction.editReply(`Your uniqueness score is ${score}%!`);
+        };
+    }
+};
